6018d18a6077047a9a483dcd5899d57d14b2860c
Brian61 <Omnivore1961@gmail.com>
2015-02-12 22:12:58 -0800

    Back to Json deep copy - reflection failed list test

--- Peanuts/JsonHelper.cs
+++ Peanuts/JsonHelper.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization.Formatters;
+﻿using System.Runtime.Serialization.Formatters;
 using Newtonsoft.Json;
 
 namespace Peanuts
@@ -40,35 +37,3 @@ namespace Peanuts
         }
     }
 }
-
-public static class ObjectExtension
-{
-    public static object DeepCloneObject(this object objSource)
-    {
-        //Get the type of source object and create a new instance of that type
-        var typeSource = objSource.GetType();
-        var objTarget = Activator.CreateInstance(typeSource);
-
-        //Get all the properties of source object type
-        var propertyInfo = typeSource.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-        //Assign all source property to taget object 's properties
-        foreach (var property in propertyInfo.Where(property => property.CanWrite))
-        {
-            //check whether property type is value type, enum or string type
-            if (property.PropertyType.IsValueType || property.PropertyType.IsEnum || property.PropertyType == typeof(String))
-            {
-                property.SetValue(objTarget, property.GetValue(objSource, null), null);
-            }
-            //else property type is object/complex types, so need to recursively call this method until the end of the tree is reached
-            else
-            {
-                var objPropertyValue = property.GetValue(objSource, null);
-                property.SetValue(objTarget, objPropertyValue == null ? null 
-                    : objPropertyValue.DeepCloneObject(), null);
-            }
-        }
-        return objTarget;
-    }
-}
-
